b7335e25081209fb6db79ff87855c3643d502438
Clif Wolfe <clifwolfe1@gmail.com>
2021-07-25 16:42:28 -0400

    Separated the run script from the build script

--- run_ghidra_build.sh
+++ run_ghidra_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t "build_ghidra" .
+rm -rf build_output
 # generate a random name so that it is not a static name
 CONTAINER_NAME=$(tr -dc A-Za-z0-9 </dev/random | head -c 13)
 docker run -ti --name "$CONTAINER_NAME" build_ghidra
